ab005c306637b1efa07be97442a4ee6b53ea8118
anonymvs <dani940414@gmail.com>
2018-04-27 03:36:41 -0700

    remove unnecessary assignment

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,7 +77,7 @@ public class GT4500 implements SpaceShip {
       } else if(firingMode == ALL) {
         // try to fire both of the torpedo stores
         if(!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()) {
-          firingSuccess = primaryTorpedoStore.fire(1);
+          primaryTorpedoStore.fire(1);
           firingSuccess = secondaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = false;
         }
